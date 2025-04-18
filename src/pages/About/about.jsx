6bd94c962d7713351d62a6fe1fd63ea8af1de7c5
Cedric Veraud <pegeco@gmail.com>
2025-04-18 19:26:06 +0200

    Mise en place des breakpoints (approche mobile-first) >>> corrections

--- src/pages/About/about.jsx
+++ src/pages/About/about.jsx
@@ -1,14 +1,23 @@
-import Banner from '../../components/banner/banner'
-import BannerImage from '../../assets/images/about-banner.jpg'
+import { useEffect, useState } from 'react'
 
+import Banner from '../../components/banner/banner'
+import Collapse from '../../components/collapse/collapse'
+
+import bannerImage from '../../assets/images/about-banner.jpg'
 import Styles from './about.module.scss'
 
 function About() {
+  const [about, setAbout] = useState([])
+
+  useEffect(() => {
+    fetch('/src/utils/data/about.json')
+      .then((res) => res.json())
+      .then((res) => setAbout(res))
+      .catch((err) => console.log('Erreur : ', err))
+  }, [])
+
   return (
     <main className={Styles.about}>
-<<<<<<< Updated upstream
-      <Banner img={BannerImage} name="Home banner" />
-=======
       <Banner
         img={bannerImage}
         name="Home banner"
@@ -26,7 +35,6 @@ function About() {
         ))}
       </div>
 
->>>>>>> Stashed changes
     </main>
   )
 }
