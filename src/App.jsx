1abbaa87f2ffc19a52f623f68704864a7f11d4f8
Cedric Veraud <pegeco@gmail.com>
2025-05-04 13:42:07 +0200

    Oublis

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom'
 // LAYOUT
-import Header from './components/header/header';
-import Footer from './components/footer/footer';
+import Header from './components/header/header'
+import Footer from './components/footer/footer'
 // PAGES
-import Home from './pages/Home/home';
-import About from './pages/About/about';
-import Logement from './pages/Logement/logement';
-import Error from './pages/Error/404';
+import Home from './pages/Home/home'
+import About from './pages/About/about'
+import Logement from './pages/Logement/logement'
+import Error from './pages/Error/404'
 
 function App() {
   return (
@@ -14,13 +14,13 @@ function App() {
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="about" element={<About />} />
+        <Route path="/about" element={<About />} />
         <Route path="/logement/:logementId" element={<Logement />} />
         <Route path="*" element={<Error />} />
       </Routes>
       <Footer />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,8 @@
 import { createRoot } from 'react-dom/client'
 import App from './App'
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom'
 
-import './style/main.module.scss';
+import './style/main.module.scss'
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
