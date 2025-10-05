bb6763798c139e0197fbc27d172935f4080d8e52
Cedric Veraud <pegeco@gmail.com>
2025-10-05 08:37:50 +0200

    deploying fails...

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,4 @@ import react from '@vitejs/plugin-react-swc';
 // https://vite.dev/config/
 export default defineConfig({
 	plugins: [react()],
-	base: 'https://cedveraud.github.io/oc-projet7_kasa-main/',
 });
